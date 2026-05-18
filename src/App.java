786f741a13882d36c68b30c71a36c475c5acdeeb
Andres <y2@Andress-MacBook-Pro.local>
2026-05-18 08:21:56 -0500

    imp

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import models.Product;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Product[] producto = {
+            new Product("Laptop",20),
+            new Product("Smartphone",25),
+            new Product("Tablet",10),
+            new Product("Monitor",5),
+            new Product("Keyboard",15),
+        };
+
+        BusquedaBinaria.sortInsec(producto);
+
+        int res = BusquedaBinaria.findByName(producto, "Laptop");
+        if(res>=0){
+            System.out.println("Entre en pos = " + res);
+        }else{
+            System.out.println("no se encontro ");
+        }
+    }   
 }
---
+++ src/BusquedaBinaria.java
@@ -0,0 +1,32 @@
+import models.Product;
+public class BusquedaBinaria {
+    public static int findByName(Product [] products,String nombre){
+        int inicio = 0;
+        int fin = products.length-1;
+        while(inicio<=fin){
+            int medio=(inicio + fin)/2;
+            if(nombre.compareTo(products[medio].getNombre())==0){
+                return medio;
+            }else if(nombre.compareTo(products[medio].getNombre())<0){
+                inicio = medio+1;
+            }else{
+                fin = medio-1;
+            }
+        }
+        //por conveciones cuando se devuelve indices y no se encuentra se debe devolver -1;
+        //pero su es busqueda por objetos se devuelve null
+        return -1;
+    }
+
+    public static void sortInsec(Product [] products){
+        for(int i = 1;i<products.length;i++){
+            Product key = products[i];
+            int j = i-1;
+            while(j>=0 && (products[j].getNombre().compareTo(key.getNombre())<0)){
+                products[j+1] = products[j]; 
+                j--;
+            }
+            products[j+1]=key;
+        }
+    }
+}
